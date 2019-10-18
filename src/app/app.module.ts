d7c8ec731ec260a20214cdbe9fe6357bb582c44b
Rogerio <rogeri_22@hotmail.com>
2019-10-18 20:54:55 -0300

    exercicio 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuscaCepComponent } from './busca-cep/busca-cep.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscaCepComponent
   ],
   imports: [
     BrowserModule,
