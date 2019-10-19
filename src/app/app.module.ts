52296a0d18d061b0901375323c0bf823c4d2b6ba
Rogerio <rogeri_22@hotmail.com>
2019-10-18 22:28:00 -0300

    exercicio 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BuscaCepComponent } from './busca-cep/busca-cep.component';
+import { ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { from } from 'rxjs';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,9 @@ import { BuscaCepComponent } from './busca-cep/busca-cep.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/busca-cep/busca-cep.component.ts
+++ src/app/busca-cep/busca-cep.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { CEP } from '../model/cep';
 
 @Component({
   selector: 'has-busca-cep',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./busca-cep.component.css']
 })
 export class BuscaCepComponent implements OnInit {
+  
+  cepForm: FormGroup;
+  cep: CEP;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    this.cepForm = this.formBuilder.group({
+      cep : this.formBuilder.control('')
+
+    })
+
+    
+  }
+  onConsultar(){
+    let cep : string = this.cepForm.value.cep;
+    alert(cep);
+
   }
 
 }
