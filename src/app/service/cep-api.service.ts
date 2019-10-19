510866af6e70de36fab4fae4e262ae22f2e29894
Rogerio <rogeri_22@hotmail.com>
2019-10-18 22:29:45 -0300

    exercicio 13

--- src/app/service/cep-api.service.ts
+++ src/app/service/cep-api.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { throwError } from 'rxjs';
+import { HttpClient } from 'selenium-webdriver/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CepAPIService {
 
-  constructor() { }
+  apiURL: string = "https://viacep.com.br/ws/";
+
+  constructor(private httpClient: HttpClient) { }
+  
+  handleError(error) {
+    let errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
 }
